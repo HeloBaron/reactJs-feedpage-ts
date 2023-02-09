ec1781ccf39a980ff5ae7fda39ec42b11d2541a3
Heloisa Baron de Athayde <88255904+HeloBaron@users.noreply.github.com>
2023-02-09 11:11:03 -0300

    first commit: Migration to JS for TS

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,28 @@
+import { Pen, PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar'
+
+import styles from "./Sidebar.module.css"
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img
+                className={styles.cover}
+                src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" alt='' />
+
+            <div className={styles.profile}>
+                <Avatar src="https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" />
+
+                <strong>Cherry Baron</strong>
+                <span>Web Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    <PencilLine />
+                    Edit
+                </a>
+            </footer>
+        </aside>
+    )
+}
